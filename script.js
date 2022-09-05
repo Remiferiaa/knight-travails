7cc8d9e976c6cf87b2993d1735dfd1022084f5e5
Remiferiaa <slong819@hotmail.com>
2022-09-05 23:03:54 +0800

    script file added

---
+++ script.js
@@ -0,0 +1,25 @@
+const knightMoves = require('./travails')
+
+knightMoves([3,3],[7,6])
+
+/* 
+Achieved in 3 moves! Heres the Path Taken:
+[ 3, 3 ]
+[ 4, 5 ]
+[ 5, 7 ]
+[ 7, 6 ]
+*/
+
+knightMoves([7,7],[0,0])
+
+/* 
+Achieved in 6 moves! Heres the Path Taken:
+[ 7, 7 ]
+[ 6, 5 ]
+[ 5, 3 ]
+[ 4, 1 ]
+[ 2, 0 ]
+[ 1, 2 ]
+[ 0, 0 ]
+*/
+
