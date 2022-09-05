57ed3458268fb48aeeb8f2b52538df7dad573d4c
Remiferiaa <slong819@hotmail.com>
2022-09-05 22:51:04 +0800

    done with the logic

--- travails.js
+++ travails.js
@@ -21,6 +21,9 @@ const knight = (value) => {
         [2, -1],
         [1, -2]
     ]
+    if(board[val[0]][val[1]] === 0) {
+        board[val[0]][val[1]] = 'V'
+    }
     for (let i = 0; i < moves.length; i++) {
         if (val[0] + moves[i][0] < 8 && val[0] + moves[i][0] >= 0
             && val[1] + moves[i][1] < 8 && val[1] + moves[i][1] >= 0) {
@@ -28,13 +31,35 @@ const knight = (value) => {
         }
 
     }
-
-    return [...child, value]
+    return [...child]
 }
 
 const knightMoves = (start, end) => {
-    let queue = [start, []]
+    let queue = [[start]]
+    while(queue) {
+        let cur = queue.shift()
+        let now = knight(cur[cur.length -1])
+        for(let i = 0; i < now.length; i++) {
+            if(now[i][0] === end[0] && now[i][1] === end[1]){
+                cur.push(now[i])
+                result(cur)
+                return 
+            }
+            let res = cur.slice(0)
+            res.push(now[i])
+            queue.push(res)
+        }  
+    }
 }
+
+const result = (res) => {
+    console.log(`Achieved in ${res.length-1} moves! Heres the Path Taken:`) 
+    for(let i = 0; i <res.length; i++) {
+        console.log(res[i])
+    }
+}
+
+
 
 
 
